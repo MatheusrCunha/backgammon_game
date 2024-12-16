084e64d1cf1f2671c334af54229c2244f2e70786
MatheusrCunha <matheus.rodriguesifsc@gmail.com>
2024-12-16 19:59:01 -0300

    adicionando board e dice
    board e dice

--- code/backgammon/board.h
+++ code/backgammon/board.h
@@ -5,8 +5,19 @@
 
 class board
 {
+
+private:
+
+
+
 public:
     board();
+    ~board();
+
+    int movepieceBlack(int x, int y);
+
+    int numpiecesBlack(int x);
+    int numpiecesWhite(int y);
 };
 
 #endif // BOARD_H
--- code/backgammon/dice.h
+++ code/backgammon/dice.h
@@ -1,10 +1,24 @@
 #ifndef DICE_H
 #define DICE_H
 
+#include <QObject.h>
+
+
 class Dice
 {
+
+    QObject;
+
+private:
+
+
 public:
     Dice();
+    ~Dice();
+
+    int rollDice();
+    int faceValue();
+
 };
 
 #endif // DICE_H
