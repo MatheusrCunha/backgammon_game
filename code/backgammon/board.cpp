dd8ef9fcae05d8b8b919191b3bd47d31bc27ee61
MatheusrCunha <matheus.rodriguesifsc@gmail.com>
2025-01-27 21:20:29 -0300

    adicionando board
    board

--- code/backgammon/board.cpp
+++ code/backgammon/board.cpp
@@ -1,22 +1,22 @@
 #include "board.h"
-#include <QBrush>
 
 Board::Board(int width, int height)
-    : boardWidth(width), boardHeight(height), boardColor(Qt::lightGray) {
+    : boardWidth(width), boardHeight(height), boardColor(Qt::lightGray) {}
+
+Board::~Board() {}
+
+void Board::draw(QPainter &painter) {
+    // Desenha o fundo do tabuleiro
+    painter.setBrush(boardColor);
+    painter.drawRect(0, 0, boardWidth, boardHeight);
+
+    // Desenha a barra central
+    drawBar(painter);
 }
 
-Board::~Board() {
-    // Nada a destruir explicitamente, mas útil se futuramente usar ponteiros
-}
-
-void Board::draw(QPainter &painter){
-
-}
-
-void Board::drawTriangles(QPainter &painter){
-
-}
-
-void Board::drawBar(QPainter &painter){
-
+void Board::drawBar(QPainter &painter) {
+    int barWidth = 20;                          // Largura da barra central
+    int barX = boardWidth / 2 - barWidth / 2;   // Posição da barra central
+    painter.setBrush(Qt::darkGray);
+    painter.drawRect(barX, 0, barWidth, boardHeight);
 }
--- code/backgammon/main.cpp
+++ code/backgammon/main.cpp
@@ -3,10 +3,26 @@
 #include <QPainter>
 #include "board.h"
 
+class GameWindow : public QWidget {
+public:
+    GameWindow() {
+        setWindowTitle("Tabuleiro de Gamão");
+        resize(900, 400);
+    }
+
+protected:
+    void paintEvent(QPaintEvent *event) override {  // Sobrescrevendo o método paintEvent
+        QPainter painter(this);  // Criando o objeto QPainter para desenhar na janela
+        board.draw(painter);     // Chamando o método de desenho do tabuleiro
+    }
+
+private:
+    Board board{900, 400};  // Instanciando o objeto Board
+};
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-
-
+    QApplication app(argc, argv);  // Inicializa a aplicação Qt
+    GameWindow window;             // Criando a janela
+    window.show();                 // Exibindo a janela
+    return app.exec();             // Iniciando o loop de eventos da aplicação
 }
-
